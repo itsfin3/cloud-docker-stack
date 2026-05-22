c699a915ba751ab0b721c8a7452b5ea4e495cf8c
itsfin3 <nazar.taran24@gmail.com>
2026-05-22 13:50:59 +0000

    add redeploy-stacks.sh utility script

---
+++ utilities/redeploy-stacks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BASE_DIR=/srv/docker # Specify the parent dir where you have moved the compose files
+
+STACKS=(
+  hobby/minecraft
+  hobby/nginx
+  monitoring/grafana
+  monitoring/prometheus
+  server-management/portainer
+  server-management/wg-easy
+)
+
+for stack in "${STACKS[@]}"; do
+  echo "Redeploying $stack..."
+  docker compose -f "$BASE_DIR/$stack/docker-compose.yml" pull
+  docker compose -f "$BASE_DIR/$stack/docker-compose.yml" up -d
+done
